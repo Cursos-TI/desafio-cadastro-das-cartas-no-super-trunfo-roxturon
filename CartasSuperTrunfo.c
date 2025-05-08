5615778fcf8102e3afd1b0b81736e0b7890a3d19
roxturon <joao.jp@live.com>
2025-05-08 12:39:51 +0000

    Trabalho Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,66 +11,66 @@ int pontos1, pontos2;
 
 printf("Carta 1: \n");
 
-/*Coleta o nome do estado
+//Coleta o nome do estado
 printf("Digite o estado: ");
 scanf("%s", &estado1);
 
-/*Coleta o código da carta com uma letra inicial seguida de número(s)
+//Coleta o código da carta com uma letra inicial seguida de número(s)
 printf("Digite o código da carta: ");
 scanf("%s", &carta1);
 
-/*Coleta o nome da cidade
+//Coleta o nome da cidade
 printf("Digite o nome da cidade: ");
 scanf("%s", &cidade1);
 
-/*Coleta um número inteiro da população da cidade
+//Coleta um número inteiro da população da cidade
 printf("Digite a população da cidade: ");
 scanf("%d", &populacao1);
 
-/*Coleta um número decimal do tamanho da Área
+//Coleta um número decimal do tamanho da Área
 printf("Digite a Área: ");
 scanf("%f", &area1);
 
-/*Coleta um número decimal do valor do PIB
+//Coleta um número decimal do valor do PIB
 printf("Digita o PIB: ");
 scanf("%f", &pib1);
 
-/*Coleta o número inteiro de pontos turísticos
+//Coleta o número inteiro de pontos turísticos
 printf("Digite o número de pontos turísticos: ");
 scanf("%d", &pontos1);
 
 
 printf("Carta 2: \n");
 
-/*Coleta o nome do estado
+//Coleta o nome do estado
 printf("Digite o estado: ");
 scanf("%s", &estado2);
 
-/*Coleta o código da carta com uma letra inicial seguida de número(s)
+//Coleta o código da carta com uma letra inicial seguida de número(s)
 printf("Digite o código da carta: ");
 scanf("%s", &carta2);
 
-/*Coleta o nome da cidade
+//Coleta o nome da cidade
 printf("Digite o nome da cidade: ");
 scanf("%s", &cidade2);
 
-/*Coleta um número inteiro da população da cidade
+//Coleta um número inteiro da população da cidade
 printf("Digite a população da cidade: ");
 scanf("%d", &populacao2);
 
-/*Coleta um número decimal do tamanho da Área
+//Coleta um número decimal do tamanho da Área
 printf("Digite a Área: ");
 scanf("%f", &area2);
 
-/*Coleta um número decimal do valor do PIB
+//Coleta um número decimal do valor do PIB
 printf("Digita o PIB: ");
 scanf("%f", &pib2);
 
-/*Coleta o número inteiro de pontos turísticos
+//Coleta o número inteiro de pontos turísticos
 printf("Digite o número de pontos turísticos: ");
 scanf("%d", &pontos2);
 
-/*Mostra os dados coletados da Carta 1
+//Mostra os dados coletados da Carta 1
 printf("Carta 1: \n");
 printf("Estado: %s\n", estado1);
 printf("Código: %s\n", carta1);
@@ -80,7 +80,7 @@ printf("Área: %0.2f km²\n", area1);
 printf("PIB: %0.2f bilhões de reais\n", pib1);
 printf("Número de Pontos Turísticos: %d\n", pontos1);
 
-/*Mostra os dados coletados da Carta 2
+//Mostra os dados coletados da Carta 2
 printf("Carta 2: \n");
 printf("Estado: %s\n", estado2);
 printf("Código: %s\n", carta2);
